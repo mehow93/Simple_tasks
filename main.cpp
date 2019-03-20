8c87a1bb5a7d4feeae7578ea870fcb6c196595c8
mehow93 <michmad93@gmail.com'git config --global user.email michmad93@gmail.comdsfbvsecho penisgit config --global user.email "michmad93@gmail.com>
2019-03-20 11:56:32 +0100

    bug fixing

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ using namespace std;
 void int_to_binary(int num){
     vector <int> output;
     int j=0;
-    while(num != 0)
+    do
     {
         j =  (num&1);
         num = num>>1;
         output.push_back(j);
-    }
+    }while(num != 0);
     for(auto p1= output.end()-1; p1 >= output.begin(); p1--)
     {
         cout<< *p1;
@@ -39,7 +39,7 @@ void int_to_binary_1(int liczba){
 }
 int main()
 {
-    int n= 17;
+    int n= 24;
     int_to_binary(n);
     cout<<endl;
     int_to_binary_1(n);
